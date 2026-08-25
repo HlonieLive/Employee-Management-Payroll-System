ba84cab99070f1d054c5a6ae8b6f123f77b9c46a
Lehlohonolo Tshabalala <sucsido@gmail.com>
2026-08-25 14:55:20 +0200

    Dapper and SQL communication implementation

---
+++ EmployeeManagementPayrollSystem/Controllers/EmployeesController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeManagementSystem.Data;
+using EmployeeManagementPayrollSystemUI.Models;
+
+namespace EmployeeManagementPayrollSystemUI.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeRepository _employeeRepository;
+
+        public EmployeesController(EmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        // GET: /Employees/
+        public IActionResult Index()
+        {
+            var employees = _employeeRepository.GetAllEmployees();
+            return View(employees);
+        }
+
+        // GET: /Employees/Details/5
+        public IActionResult Details(int id)
+        {
+            var employee = _employeeRepository.GetEmployee(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: /Employees/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Employees/CreateConfirmed
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateConfirmed(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", employee);
+            }
+
+            _employeeRepository.Create(employee);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: /Employees/Update/5
+        public IActionResult Update(int id)
+        {
+            var employee = _employeeRepository.GetEmployee(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: /Employees/UpdateConfirmed
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateConfirmed(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Update", employee);
+            }
+
+            _employeeRepository.UpdateEmployee(employee);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: /Employees/Delete/5 (Loads confirmation page)
+        public IActionResult Delete(int id)
+        {
+            var employee = _employeeRepository.GetEmployee(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: /Employees/DeleteConfirmed
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _employeeRepository.DeleteEmployee(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- EmployeeManagementPayrollSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EmployeeManagementPayrollSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
